74c3587177d5ccc93003bdda34f594ef6d43ea71
Meruert <mika.amantaikuzu@gmail.com>
2025-01-05 11:51:40 +0500

    feat: complete 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,21 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const pathToFile = path.join(__dirname, 'text.txt');
+const rs = fs.createReadStream(pathToFile, 'utf-8');
+
+rs.on('data', (chunk) => {
+  console.log(chunk);
+});
+
+rs.on('error', (err) => {
+  if (err.code === 'ENOENT') {
+    console.log('Wrong path!');
+  } else {
+    console.log('Something wrong!', err);
+  }
+});
+
+rs.on('end', () => {
+  console.log("\nThat's all!");
+});
